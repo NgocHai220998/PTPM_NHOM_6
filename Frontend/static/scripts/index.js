3f7444ebeb386439b33e7334e0d902c44bd05ece
huongtrauet <huongtrauet@gmail.com>
2020-11-08 18:06:32 +0700

    vuex socket

--- Frontend/static/scripts/index.js
+++ Frontend/static/scripts/index.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
   console.log(window.innerHeight)
   const chatBox = document.getElementsByClassName('sidebar-left-chatbox')[0]
   const chatBoxContent = document.getElementsByClassName('chatbox-content')[0]
-  const boxEnoji = document.getElementsByClassName('emoji-picker-tabs')[0]
   chatBox.style.height = (windowHieght - 335) + 'px'
   chatBoxContent.style.height = windowHieght - 330 - 65 + 'px'
 })
