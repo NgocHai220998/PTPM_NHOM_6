e2dbc92cc9376047a5ed096a3fb82ebfc185b4ac
HuyHoang <tranhuyhoang8352@gmail.com>
2020-11-13 21:25:33 +0700

    Update config.yml

--- Socket/src/connection/databaseConnection.js
+++ Socket/src/connection/databaseConnection.js
@@ -7,8 +7,7 @@ module.exports = () => {
     if (process.env.NODE_ENV === 'production') {
         mongoose.connect(`mongodb://${configDatabase.username}:${configDatabase.password}@ds157829.mlab.com:57829/back-end`, {useNewUrlParser : true});
     } else {
-        // mongoose.connect(url, { useNewUrlParser: true })
-        mongoose.connect(`mongodb://${configDatabase.username}:${configDatabase.password}@ds157829.mlab.com:57829/back-end`, {useNewUrlParser : true});
+        mongoose.connect(url, { useNewUrlParser: true })
     }
 }
 
